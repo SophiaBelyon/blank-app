f7ddf3cf3d91da9461e128a7c27f9d151527d723
SophiaBelyon <Sophia.Belyon@strathmore.edu>
2025-06-08 15:54:04 +0300

    group edit

--- WebApp.py
+++ WebApp.py
@@ -17,7 +17,7 @@ with col2:
     st.write('') 
     #setting the title of the web app   
 with col3:
-    st.title("ⴍage") 
+    st.title("JANS") 
     #setting column 4 of the web app
 with col4:
     st.write('')
@@ -35,17 +35,17 @@ with col8:
 with col9:
     st.write('')
 #Lists defining options for user input
-gen_list = ["Female", "Male"]
-edu_list = ["Bachelor's", "Master's", "PhD"]
+gen_list = ["Female", "Male", "Other"]	
+edu_list = ["Certificate", "Diploma" , "Bachelor's", "Master's", "PhD"]
 job_list = ["Director of Marketing", "Director of Operations", "Senior Data Scientist", "Senior Financial Analyst", "Senior Software Engineer"]
 #Maps the job titles to numeric values(Corresponding indices for job titles)
 job_idx = [0, 1, 10, 11, 20]
 #Creates input fields for user data
 gender = st.radio('Pick your gender', gen_list)# Radio Buttons to select gender
-age = st.slider('Pick your age', 21, 55)# Slider to select age btwn 21 and 55
+age = st.slider('Pick your age', 21, 65)# Slider to select age btwn 21 and 55
 education = st.selectbox('Pick your education level', edu_list)# Dropdown to select education level
 job = st.selectbox('Pick your job title', job_list)# Dropdown to select job title
-experience = st.slider('Pick your years of experience', 0.0, 25.0, 0.0, 0.5, "%1f")# Slider to select years of experience(from 0 to 25 years)with 0.5 year increments
+experience = st.slider('Pick your years of experience', 0.0, 40.0, 0.0, 0.5, "%1f")# Slider to select years of experience(from 0 to 25 years)with 0.5 year increments
 # Creates a 5 column layout used to center the 'Predict Salary'button
 col10, col11, col12, col13, col14 = st.columns(5)
 with col10:
